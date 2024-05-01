1e3fc998700ce08e0334f396c7ab4c7fccc20d4e
naseemnaz12 <164153238+naseemnaz12@users.noreply.github.com>
2024-05-01 16:04:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// consol.log("Naseem naz");
+// alert(" This is not working");
+// alert("Welcome to our website");
+console.log("careful writers ends everery declarative sentence with a period")
+
+var first_name ="Naseem";
+console.log(first_name);
+var first_name ="Naz";
+console.log(first_name);
+
+// let nick_name ="Naz";
+// console.log(Naz);
+// let nick_name ="seher";
+// console.log(seher);       <p>error will be found in that case</p>
+
+
+var first_name;  //declare
+first_name = "Naseem ";
+var first_name="Naseem";
+
+var number ="2024";
+console.log(number);
+
+var text =("It is invented in  1985");
+console.log(text);
+
+var name ="Naseem Naz";
+var email ="raparolla66@gmail.com";
+var dateofbirth ="2000";
+console.log(name+"  "+email+"   "+dateofbirth);
+
+alert(name+"  "+email+"   "+dateofbirth);
+
+let a = 12;
+let b = 13;
+let result = a+b;
+console.log("The sum of two number is:"+(a + b));
